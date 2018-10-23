986f0e75cb2621ee318aaaeb6317b8a5b7acee0b
stevenjmarsh <sjm.atl@gmail.com>
2018-10-23 16:56:58 -0400

    displaying a simple react component, adding react packages, configuring babel presets

--- src/main.js
+++ src/main.js
@@ -1,2 +1,18 @@
-const $ = require("jquery");
-$("#target").html("Hello World!!!!");
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class Message extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.message}</p>
+      </div>
+    );
+  }
+}
+
+render(
+  <Message title="Email Joe" message="Can you email him?" />, 
+  document.getElementById("react-container")
+);
